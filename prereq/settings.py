43d5a81530faf494f0df10580030bd3a981f7adf
ranacenas <36704956+ranacenas@users.noreply.github.com>
2018-11-09 00:05:27 -0500

    config try 2

--- prereq/settings.py
+++ prereq/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('UofT_Secret')
 
-API_KEY = config("key")
+API_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
