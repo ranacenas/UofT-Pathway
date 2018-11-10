ea19f6b7ef868f34c822e47f19b33abc942ef862
ranacenas <36704956+ranacenas@users.noreply.github.com>
2018-11-10 10:55:31 -0500

    Made the code much more readable and understandable - I hope

--- UofT/forms.py
+++ UofT/forms.py
@@ -3,11 +3,21 @@ from django.conf import settings
 import requests
 from decouple import config
 
+#API Key
 API_KEY = config("API_KEY")
+
 class CourseForm(forms.Form):
     course = forms.CharField(max_length=10)
 
     def catch_rel(self):
+      """type(self) = list
+      
+      returns a list with strings containing the Codes. 
+      This function is usually used after the check function 
+      (see below)
+      
+      catch_rel(['Completion', 'of', 'BIO325H1F']
+      => ['BIO325H1F'] """"
       Codes = ['H1', 'H3', 'H5', 'Y']
       catch = []
       for course in self:
@@ -21,8 +31,12 @@ class CourseForm(forms.Form):
 
 
     
-
     def check(self):
+      """type(self) = list
+      generates a new list
+      
+      check(['MATA36H3F and BIOA01H3F', 'Completion of CSC411H1F/CSC404H1F'])
+      => ['MATA36H3', 'BIOA01H3', 'CSC411H1', 'CSC404H1F']"""
       L1_pre = self.replace("/", " ").split()
       strip_and = [code for code in L1_pre if code != "and"]
       strip_or = [code for code in strip_and if code != "or"]
@@ -45,6 +59,10 @@ class CourseForm(forms.Form):
       return L1_pre
     
     def second_search(self):
+      """type(self) = string 
+      returns a dictionary
+      
+      searches the course code and returns a dictionary"""
       
       url = """https://cobalt.qas.im/api/1.0/courses/filter?q=term:"{term}"%20AND%20code:"{code}"%20&key={key}"""
       urlfinal = url.format(code= self, term = 2018, key = API_KEY)
@@ -52,12 +70,20 @@ class CourseForm(forms.Form):
       result_preq = response.json()
       if len(result_preq) == 0:
         result_preq = "none"
-        
       else:
         result_preq = result_preq[0]
       return result_preq
     
     def PRQ(self):
+      """type(self) = dictionary
+
+      PRQ(dict):
+      returns string
+
+      This function is used after the catch_rel function
+
+      If the dictionary is not empty (meaning the course code exists)
+      it takes the prerequisites information from that dictionary and returns a string."""
       if self != 'none':
         PRE = self["prerequisites"]
       else:
@@ -66,42 +92,25 @@ class CourseForm(forms.Form):
 
 
     def search(self):
+      """User inputs a course code and returns a dictionary"""
       course = self.cleaned_data['course']
       url = """https://cobalt.qas.im/api/1.0/courses/filter?q=term:"{term}"%20AND%20code:"{code}"%20&key={key}"""
       urlfinal = url.format(code= course, term = 2018, key = API_KEY)
-      response = requests.get(urlfinal)
-      result = response.json()
-
-#       NO PREREQUISITE / ERROR 
+      response = requests.get(urlfinal) 
+      result = response.json() #returns a dictionary IN a list
       if len(result) == 0:
-              Level_one = "sorry, try again"
-              pass
+        Level_one = "sorry, try again"
+        pass
 
-#       A LEVEL PREREQUISITE
       else:
-          Level_one = result[0]
-          
+          Level_one = result[0]  
       return Level_one
 
 
 class AutoTree(dict):
-
+  #This helps make nested dictionaries.
   def __missing__(self, key):
     value = self[key] = type(self)()
     return value
 
-class Loop(dict):
-
-  def Loop(self, list):
-    master_list = []
-    for i in list:
-      S2 = CourseForm.second_search(i)
-      S2 = S2["prerequisites"]
-      S2 = CourseForm.check(S2)
-      master_list.append(S2)
-    return master_list
-  
-  def dict(self, one, two):
-    dictionary = dict(zip(one, two))
-    return dictionary
 
--- UofT/views.py
+++ UofT/views.py
@@ -4,71 +4,53 @@ from .forms import CourseForm, AutoTree, Loop
 # Create your views here.
 
 def index(request):
-    step_two = {}
-    prereq_list = {}
+    
     nest = AutoTree()
-    loop = Loop()
     if 'course' in request.GET:
         form = CourseForm(request.GET)
         if form.is_valid():
-            #SEARCHES THE COURSE CODE
-            SEARCH_RESULT = form.search()
-            if SEARCH_RESULT == 'sorry, try again':
+            SEARCH_RESULT = form.search() #SEARCHES THE COURSE CODE
+            if SEARCH_RESULT == 'sorry, try again':  #if course code does not exist
                 nest['code'] = 'Sorry try again, the course information cannot be found'
             
             else:
-
-                recent =  SEARCH_RESULT['id']
-
+                recent =  SEARCH_RESULT['id'] 
                 nest['id'] = recent
 
                 
+                COURSE_CODE = SEARCH_RESULT['code'] #TAKES ONLY THE CODE VALUE from dict
+                nest['code'] = COURSE_CODE #adds it into the dictionary 
+                PREREQUISITE_INITIAL = SEARCH_RESULT['prerequisites'] #TAKES THE PREREQUISITE OF THE RESULT
+                
 
-                #RETURNS RESULT AND TAKES ONLY THE CODE VALUE
-                COURSE_CODE = SEARCH_RESULT['code']
-
-                #TAKES THE PREREQUISITE OF THE RESULT
-                PREREQUISITE_INITIAL = SEARCH_RESULT['prerequisites']
-                nest['code'] = COURSE_CODE
-
-                if len(PREREQUISITE_INITIAL) == 0:
+                if len(PREREQUISITE_INITIAL) == 0: 
                     nest['prerequisites']['prerequisites_zero'] = "Prerequisites were not found for this course"
                 else:
+                    Prereq_list = CourseForm.check(PREREQUISITE_INITIAL)  #STRIPS AND TURNS THE PREREQUISITES INTO A LIST
 
-                #STRIPS AND TURNS THE PREREQUISITES INTO A LIST
-                    Prereq_list = CourseForm.check(PREREQUISITE_INITIAL)
-
-                    #ADDS IT INTO THE DICTIONARY
-                    
-                    nest['prerequisites']['prerequisites_zero'] = PREREQUISITE_INITIAL
+                    nest['prerequisites']['prerequisites_zero'] = PREREQUISITE_INITIAL #ADDS IT INTO THE DICTIONARY
                     
 
-                    #Campus codes filters
-                    CAMPUS = ["H3", "H1", "H5", "Y1"]
-
-                    #New list of course codes
-
-                    P1 = CourseForm.catch_rel(Prereq_list)
+                    P1 = CourseForm.catch_rel(Prereq_list) #Generates a list containing only course codes
                     nest['P1'] = P1
 
                     if len(P1) > 0:
-                        P2_list = []
-                        P2_all = []
+                        P2_list = [] #Once the loop finishes, it returns a list of lists with the prerequisites split.
+                        P2_all = [] #returns a list of lists of the prerequisites - unsplit
                         
                         for course in P1:
-                            P2_search = CourseForm.second_search(course)
-
-                            P2_pre = CourseForm.PRQ(P2_search)
+                            P2_search = CourseForm.second_search(course) #searches each course code in P1
+                            P2_pre = CourseForm.PRQ(P2_search) #Takes the prerequisites if they exist. Returns a long string
                             
                             if P2_pre == 'none':
                                 pass
                             else:
-                                P2_all.append(P2_pre)
+                                P2_all.append(P2_pre) 
                                 
-                                P2_check = CourseForm.check(P2_pre)
+                                P2_check = CourseForm.check(P2_pre) #Splits the prerequisites
                                 P2_list.append(P2_check)
 
-                                P2_zip = dict(zip(P1, P2_list))
+                                P2_zip = dict(zip(P1, P2_list)) #zips the course code and its corresponding prerequisites
                                 P2_all_zip = dict(zip(P1, P2_all))
                                 
                                 nest['P2_all'] = P2_all_zip
@@ -78,43 +60,30 @@ def index(request):
                             pass
                             
                         P3_list = []
-                        
-                        #P3 = {}
-                        for num in range(len(P2_list)):
-                            P3_catch = CourseForm.catch_rel(P2_list[num])
-                            
-                            L = [x for x in P3_catch if CourseForm.PRQ(CourseForm.second_search(x)) != 'none']
+                        for num in range(len(P2_list)): #P2 list is multidimensional 
+                            P3_catch = CourseForm.catch_rel(P2_list[num]) #filters out P2 list to ensure only course codes are in the new list
+                            L = [x for x in P3_catch if CourseForm.PRQ(CourseForm.second_search(x)) != 'none'] #checks if prerequisites exist
                             P3_list.append(L)
-                            nest['LLLLLLLLLLLLLLLLLL'] = P3_list
+                            nest['P3_listt'] = P3_list #multidimensional list
                             pass
                             
-                                
-                            
-                            
-                        
-
-
-                        
-                        P3_final = [[] for items in range(len(P3_list))]
+        
+                        P3_final = [[] for items in range(len(P3_list))] #multidimensional list depending on the dimensions of P3_list
                         for i in range(len(P3_list)):
-                            
                             for q in P3_list[i]:
-                                P3_search = CourseForm.second_search(q)
-                                
-
-                                
-                                if P3_search != 'none':
+                                P3_search = CourseForm.second_search(q) 
+                                if P3_search != 'none': 
                                     P3_Pre = P3_search["prerequisites"]
                                 
                                     
-                                    P3_final[i].append(P3_Pre)
-                                    nest['PEEEEEEE'] = P3_final
+                                    P3_final[i].append(P3_Pre) #appends to corresponding list in the index <- did I use the right terms here?
+                                    nest['P3_final'] = P3_final
                                     pass
                                 
 
                         P3_dict = {}          
                         for i in range(len(P3_final)):
-                            P3_dict.update(dict(zip(P3_list[i], P3_final[i])))
+                            P3_dict.update(dict(zip(P3_list[i], P3_final[i]))) #Update = removes duplicates = bad. Taken care of below
                             nest["P3_dict"] = P3_dict
                             continue
                         for j, k in P2_zip.items():
@@ -125,27 +94,10 @@ def index(request):
                                     else:
                                         pass
                                      
-
-                       
-
-
-
-
-                
-            
-
-
-
     else:
-        form = CourseForm()
-        preq = CourseForm()
-    return render(request, 'UofT/UofTpreReq.html', {'form': form, 'nest':nest})
+        form = CourseForm() #if invalid, it reverts back here. important.
 
-# def test(request):
-#     if request.method == 'GET':step_three_
-#         h = .get('get_get')
-#         step = CourseForm.second_search(h)
-#         return render(request,'uoftapp/UofTpreReq.html', {'ste': step} )
+    return render(request, 'UofT/UofTpreReq.html', {'form': form, 'nest':nest})
 
 
 
